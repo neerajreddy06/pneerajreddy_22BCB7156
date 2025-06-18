ebee45a41ac911f459e2462fb7afe4b65fb0af1c
P NEERAJ REDDY <neeraj.22bcb7156@vitapstudent.ac.in>
2025-06-18 14:05:03 +0530

    Add files via upload

---
+++ MongoDB-Project/ecommerce-app/client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
